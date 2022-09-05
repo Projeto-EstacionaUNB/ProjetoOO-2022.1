17a00dba25d312198726ea75cef7b992031b9283
paulohborba <paulohbr18@gmail.com>
2022-09-04 23:17:59 -0300

    Mudanca evento

--- Projeto-OO/src/model/Evento.java
+++ Projeto-OO/src/model/Evento.java
@@ -1,35 +1,16 @@
 package model;
 
-public class Evento extends Acesso{
-    private double valorEvento;
+public class Evento {
+    public Evento(double valorEvento, String dataEvento) {
+		super();
+		this.valorEvento = valorEvento;
+		this.dataEvento = dataEvento;
+	}
+	public Evento() {
+		super();
+	}
+	private double valorEvento;
     private String dataEvento;
 
-    public Evento() {
-    }
-
-    public Evento(String horaEntrada, String horaSaida, String dataInicial, String dataFinal, Veiculo veiculoCliente, double valorEvento, String dataEvento) {
-        super(horaEntrada, horaSaida, dataInicial, dataFinal, veiculoCliente);
-        this.valorEvento = valorEvento;
-        this.dataEvento = dataEvento;
-    }
-
-    // Vericar Correção
-
-    @Override
-    public double calculaValorAcesso() {
-        return 0;
-    }
-
-    @Override
-    public String toString() {
-        return "Evento{" +
-                "valorEvento=" + valorEvento +
-                ", dataEvento='" + dataEvento + '\'' +
-                ", horaEntrada='" + horaEntrada + '\'' +
-                ", horaSaida='" + horaSaida + '\'' +
-                ", dataInicial='" + dataInicial + '\'' +
-                ", dataFinal='" + dataFinal + '\'' +
-                ", veiculoCliente=" + veiculoCliente +
-                '}';
-    }
+   
 }
